08fd823b1fcb63fa0a9f68f21549928cccab6828
Omar breich <omar_breich@epam.com>
2026-02-22 00:14:04 +0200

    Add .gitignore and implement main function in app.py

--- agent/app.py
+++ agent/app.py
@@ -5,6 +5,7 @@ import os
 from agent.mcp_client import MCPClient
 from agent.dial_client import DialClient
 from agent.models.message import Message, Role
+from agent.prompts import SYSTEM_PROMPT
 
 async def main():
 
@@ -18,7 +19,15 @@ async def main():
     #       - mcp_client=mcp_client
     # 4. Create list with messages and add there SYSTEM_PROMPT with instructions to LLM
     # 5. Create console chat (infinite loop + ability to exit from chat + preserve message history after the call to dial client)
-    raise NotImplementedError()
+    with MCPClient(docker_image="mcp/duckduckgo:latest") as mcp_client:
+        tools = await mcp_client.get_tools()
+        dial_client = DialClient(api_key=os.getenv("DIAL_API_KEY"), endpoint=os.getenv("DIAL_ENDPOINT"), tools=tools, mcp_client=mcp_client)
+        messages = [Message(role=Role.SYSTEM, content=SYSTEM_PROMPT)]
+        while True:
+            user_input = input("You: ")
+            messages.append(Message(role=Role.USER, content=user_input))
+            dial_message = await dial_client.get_completion(messages)
+            messages.append(dial_message)
 
 
 if __name__ == "__main__":
